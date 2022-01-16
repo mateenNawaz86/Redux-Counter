dd22d3ed9f7fa2a613993170af6433bb2139e80a
Mateen Mirani <mateenmirani125@gmail.com>
2022-01-16 09:29:32 +0500

    add dispatch action functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,10 +1,22 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import classes from "./Counter.module.css";
 
 const Counter = () => {
   // use redux store state in this component
   const counter = useSelector((state) => state.counter);
+
+  // function for dispatch an action
+  const dispatch = useDispatch();
+
+  // function for increment action
+  const inrementHandler = () => {
+    dispatch({ type: "INCREAMENT" });
+  };
+
+  const dcrementHandler = () => {
+    dispatch({ type: "DECREAMENT" });
+  };
 
   const toggleCounterHandler = () => {};
 
@@ -12,6 +24,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={inrementHandler}>Inrement</button>
+        <button onClick={dcrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
